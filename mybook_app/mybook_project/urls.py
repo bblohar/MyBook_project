58e4418aee7f96f5c7ecc121e250eb5f429561e7
bblohar <bhaveshblohar25@gmail.com>
2025-10-14 18:54:51 +0530

    .

--- mybook_app/mybook_project/urls.py
+++ mybook_app/mybook_project/urls.py
@@ -4,17 +4,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 # All drf_yasg related imports and schema_view definition are removed.
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.views.generic.base import RedirectView
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
     path('', include('books.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
-
-
 ]
